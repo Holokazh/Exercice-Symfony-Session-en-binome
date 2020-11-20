34ca1d986c7e0a5d5872365c4d7646fa657756b1
Holokazh <virgile.tomadon@orange.fr>
2020-11-20 17:07:11 +0100

    Ajout du formulaire d'ajout d'une catégorie + modification

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -2,14 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Module;
+use App\Entity\Category;
+use App\Repository\ModuleRepository;
+use App\Form\CategoryType;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-use App\Entity\Category;
-use App\Repository\CategoryRepository;
-use App\Entity\Module;
-use App\Repository\ModuleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProgramController extends AbstractController
 {
@@ -37,6 +40,33 @@ class ProgramController extends AbstractController
 
         return $this->render('category/list.html.twig', [
             'categories' => $categories,
+        ]);
+    }
+
+    /**
+     * @Route("/add", name="category_add")
+     * @Route("/{id}/edit", name="category_edit")
+     */
+    public function new_update(Category $category = null, Request $request, EntityManagerInterface $manager)
+    {
+        if (!$category) {
+            $category = new Category();
+        }
+
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($category);
+            $manager->flush();
+
+            return $this->redirectToRoute('listAllCategories');
+        }
+
+        return $this->render('category/add_edit.html.twig', [
+            'formCategory' => $form->createView(),
+            'editMode' => $category->getId() !== null,
+            'category' => $category->getName()
         ]);
     }
 
