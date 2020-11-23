10866bf5b0b815d07f5ea3f9fa17c2229ed0aebc
Holokazh <virgile.tomadon@orange.fr>
2020-11-23 13:31:43 +0100

    Formulaire d'ajout d'un stagiaire

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Student;
+use App\Form\StudentType;
+use App\Repository\StudentRepository;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-use App\Entity\Student;
-use App\Repository\StudentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StudentController extends AbstractController
 {
@@ -33,5 +36,51 @@ class StudentController extends AbstractController
         return $this->render('student/list.html.twig', [
             'students' => $students,
         ]);
+    }
+
+    /**
+     * @Route("/student/add", name="student_add")
+     * @Route("/student/{id}/edit", name="student_edit")
+     */
+    public function new_update(Student $student = null, Request $request, EntityManagerInterface $manager)
+    {
+        if (!$student) {
+            $student = new Student();
+        }
+
+        $form = $this->createForm(StudentType::class, $student);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($student);
+            $manager->flush();
+
+            return $this->redirectToRoute('listAllStudents');
+        }
+
+        return $this->render('student/add_edit.html.twig', [
+            'formStudent' => $form->createView(),
+            'editMode' => $student->getId() !== null,
+            'student' => $student->getFirstName()
+        ]);
+    }
+
+    /**
+     * @Route("/student/{id}/delete", name="student_delete")
+     */
+    public function deletestudent(Student $student = null, EntityManagerInterface $manager)
+    {
+        $manager->remove($student);
+        $manager->flush();
+
+        return $this->redirectToRoute('listAllStudents');
+    }
+
+    /**
+     * @Route("/student/{id}/show", name="student_show")
+     */
+    public function show(Student $student): Response
+    {
+        return $this->render('student/show.html.twig', ['student' => $student]);
     }
 }
---
+++ src/Form/StudentType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Student;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+class StudentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', ChoiceType::class, [
+                'choices' => [
+                    'Mr' => 'Mr',
+                    'Mme' => 'Mme',
+                ],
+                'label' => 'Civilité'
+            ])
+
+            ->add('firstName', TextType::class, ['label' => 'Prénom'])
+
+            ->add('lastName', TextType::class, ['label' => 'Nom'])
+
+            ->add('birthDay', BirthdayType::class, ['label' => 'Date de naissance', 'format' => 'ddMMyyyy', 'years' => range(date("Y") - 18, date("Y") - 120)])
+
+            ->add('email', EmailType::class)
+
+            ->add('city', TextType::class, ['label' => 'Ville'])
+
+            ->add('phoneNumber', TextType::class, ['label' => 'Téléphone'])
+
+            ->add('valider', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Student::class,
+        ]);
+    }
+}
