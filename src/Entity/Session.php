05dab2361094018a2251b7a5cac923f6347fab8a
Bapt890 <bapt890@gmail.com>
2020-11-25 09:55:09 +0100

    Rework de la liste des sessions + Assert NbSpace

--- src/Entity/Session.php
+++ src/Entity/Session.php
@@ -6,6 +6,7 @@ use App\Repository\SessionRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=SessionRepository::class)
@@ -21,6 +22,7 @@ class Session
 
     /**
      * @ORM\Column(type="integer")
+     * @Assert\Range(min=1, minMessage = "Il doit y avoir au moins une place.")
      */
     private $nbSpace;
 
